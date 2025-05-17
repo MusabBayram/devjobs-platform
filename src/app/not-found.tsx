2844de10ebff4cf54368ab9260610e17ddc1c850
Musab Bahadır Bayram <musab.bayram@hotmail.com>
2025-05-17 21:57:06 +0300

    file tree design created

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+// src/app/not-found.tsx
+
+export default function NotFound() {
+  return (
+    <div className="min-h-screen flex items-center justify-center text-2xl text-red-500">
+      404 | Page Not Found
+    </div>
+  );
+}
