f3a251a09b02d1a1160b4a35436ff3546a9c2475
Musab Bahadır Bayram <musab.bayram@hotmail.com>
2025-05-20 21:59:00 +0300

    Update BottomBar.tsx

---
+++ src/components/BottomBar.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import Link from "next/link";
+import {
+  HomeIcon,
+  UsersIcon,
+  PlusCircleIcon,
+  BellIcon,
+  BriefcaseIcon,
+} from "@heroicons/react/24/outline";
+
+export default function BottomBar() {
+  return (
+    <nav className="sticky bottom-0 left-0 right-0 bg-white dark:bg-zinc-900 border-t border-gray-200 dark:border-zinc-700 flex justify-around items-center py-2 z-50 md:hidden">
+      <Link
+        href="#"
+        className="flex flex-col items-center text-xs text-gray-700 dark:text-gray-300"
+      >
+        <HomeIcon className="h-6 w-6" />
+        <span>Home</span>
+      </Link>
+      <Link
+        href="#"
+        className="flex flex-col items-center text-xs text-gray-700 dark:text-gray-300"
+      >
+        <UsersIcon className="h-6 w-6" />
+        <span>My Network</span>
+      </Link>
+      <Link
+        href="#"
+        className="flex flex-col items-center text-xs text-gray-700 dark:text-gray-300"
+      >
+        <PlusCircleIcon className="h-6 w-6" />
+        <span>Post</span>
+      </Link>
+      <Link
+        href="#"
+        className="relative flex flex-col items-center text-xs text-gray-700 dark:text-gray-300"
+      >
+        <BellIcon className="h-6 w-6" />
+        <span>Notifications</span>
+        <span className="absolute top-0 right-0 transform translate-x-1/2 -translate-y-1/2 bg-red-600 text-white text-[10px] rounded-full px-1">
+          7
+        </span>
+      </Link>
+      <Link
+        href="#"
+        className="flex flex-col items-center text-xs text-gray-700 dark:text-gray-300"
+      >
+        <BriefcaseIcon className="h-6 w-6" />
+        <span>Jobs</span>
+      </Link>
+    </nav>
+  );
+}
