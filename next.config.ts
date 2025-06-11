eadae9affed11cb4d574dde98560eb3e476ff3a6
Musab Bahadır Bayram <musab.bayram@hotmail.com>
2025-06-11 20:56:16 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+  trailingSlash: true, // 📛 önemli
   images: {
+    unoptimized: true, // ❗ bu satır hatayı çözer
     remotePatterns: [
       {
         protocol: 'https',
